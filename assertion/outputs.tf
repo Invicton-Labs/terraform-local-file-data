acd72a88f514be207dc30155e4a917efe295c253
Kyle Kotowick <kotowick@invictonlabs.com>
2024-09-10 14:15:49 -0400

    Try simplifying checks

--- assertion/outputs.tf
+++ assertion/outputs.tf
@@ -8,10 +8,7 @@ output "condition" {
   value       = var.condition
 }
 
-resource "terraform_data" "assertion" {
-  input = var.condition
-}
 output "checked" {
   description = "Whether the condition has passed validation (used for assertion dependencies)."
-  value       = terraform_data.assertion.output != "" ? true : false
+  value       = var.condition ? true : true
 }
