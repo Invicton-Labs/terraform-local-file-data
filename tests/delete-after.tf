fb1e1f70d339ee170107ebf0181f844f57663f53
Kyle Kotowick <kotowick@invictonlabs.com>
2024-09-10 14:48:28 -0400

    Re-enable deleted check

--- tests/delete-after.tf
+++ tests/delete-after.tf
@@ -22,14 +22,14 @@ module "check_delete_after_exists" {
 resource "terraform_data" "bootstrap" {
   input = module.delete_after.complete
 }
-# module "check_delete_after_deleted" {
-#   source = "../assertion"
-#   # source  = "Invicton-Labs/assertion/null"
-#   # version = "0.2.4"
-#   depends_on = [
-#     // This forces a wait until the deletion has been completed as well
-#     module.delete_after
-#   ]
-#   condition     = !fileexists(module.delete_after.complete ? module.delete_after.filename : "")
-#   error_message = "delete-after (deleted): expected file to be deleted, but it exists"
-# }
+module "check_delete_after_deleted" {
+  source = "../assertion"
+  # source  = "Invicton-Labs/assertion/null"
+  # version = "0.2.4"
+  depends_on = [
+    // This forces a wait until the deletion has been completed as well
+    module.delete_after
+  ]
+  condition     = !fileexists(module.delete_after.complete ? module.delete_after.filename : "")
+  error_message = "delete-after (deleted): expected file to be deleted, but it exists"
+}
