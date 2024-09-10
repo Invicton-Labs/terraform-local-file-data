9ccfc370b7d6de0797ebf3a81f37a604ecddb18c
Kyle Kotowick <kotowick@invictonlabs.com>
2024-09-10 14:07:44 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -127,7 +127,7 @@ variable "delete_after" {
 
   This is useful if you need to write out a temporary file that is used by other shell scripts, such as credential files.
 EOF
-  type        = list(any)
+  type        = any
   default     = []
   nullable    = false
 }
