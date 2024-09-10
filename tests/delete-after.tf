0710fe48090ef0253274f0f27de3ee2f76f083d2
Kyle Kotowick <kotowick@invictonlabs.com>
2024-09-10 13:08:32 -0400

    Testing

--- tests/delete-after.tf
+++ tests/delete-after.tf
@@ -14,16 +14,16 @@ module "check_delete_after_exists" {
   version = "~>0.2.5"
   // The ternary forces a wait until the file has been created
   condition     = fileexists(module.delete_after.created ? module.delete_after.filename : "")
-  error_message = "delete-after (exists): expected file to exist, but it does not; num chunks: ${module.delete_after.num_chunks}, created: ${module.delete_after.created}, filename: ${module.delete_after.filename}, datasource: ${jsonencode(module.delete_after.datasource)}, files: ${jsonencode(fileset(module.delete_after.created ? "./../" : null, "**"))}"
+  error_message = "delete-after (exists): expected file to exist, but it does not"
 }
 
-module "check_delete_after_deleted" {
-  source  = "Invicton-Labs/assertion/null"
-  version = "~>0.2.5"
-  depends_on = [
-    // This forces a wait until the deletion has been completed as well
-    module.delete_after
-  ]
-  condition     = !fileexists(module.delete_after.created ? module.delete_after.filename : "")
-  error_message = "delete-after (deleted): expected file to be deleted, but it exists"
-}
+# module "check_delete_after_deleted" {
+#   source  = "Invicton-Labs/assertion/null"
+#   version = "~>0.2.5"
+#   depends_on = [
+#     // This forces a wait until the deletion has been completed as well
+#     module.delete_after
+#   ]
+#   condition     = !fileexists(module.delete_after.created ? module.delete_after.filename : "")
+#   error_message = "delete-after (deleted): expected file to be deleted, but it exists"
+# }
