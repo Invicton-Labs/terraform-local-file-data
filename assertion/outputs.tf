fd6e72d46b801be44575ed4db638b7de314d4245
Kyle Kotowick <kotowick@invictonlabs.com>
2024-09-10 13:37:23 -0400

    Update outputs.tf

--- assertion/outputs.tf
+++ assertion/outputs.tf
@@ -8,7 +8,10 @@ output "condition" {
   value       = var.condition
 }
 
+locals {
+  condition = var.condition
+}
 output "checked" {
   description = "Whether the condition has passed validation (used for assertion dependencies)."
-  value       = var.condition
+  value       = local.condition == true ? true : true
 }
