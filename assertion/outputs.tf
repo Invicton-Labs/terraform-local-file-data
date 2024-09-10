853d87f6cf50f187f84ebffade5af74375768cf7
Kyle Kotowick <kotowick@invictonlabs.com>
2024-09-10 16:03:17 -0400

    Update outputs.tf

--- assertion/outputs.tf
+++ assertion/outputs.tf
@@ -10,5 +10,5 @@ output "condition" {
 
 output "checked" {
   description = "Whether the condition has passed validation (used for assertion dependencies)."
-  value       = var.condition ? true : true
+  value       = md5(tostring(var.condition)) == md5("") ? true : true
 }
